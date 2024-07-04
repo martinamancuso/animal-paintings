d52be8fd7c28424bf7bbc9e377ac99257c39e1a7
martinamancuso <martinamancuso24@gmail.com>
2024-07-04 11:56:08 +0200

    feat: create mat-card for products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 
 import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ProductModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ProductModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
